188bbbac91cec4040ccfb0f7aba1e25ffae1381f
leen <leenodeh287@gmail.com>
2023-10-13 18:25:11 +0300

    settings page is done

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -1,9 +1,19 @@
-
+import './Settings.css'
 
 const Settings = () => {
   return (
-    <div>
-      settings
+    <div className='setting'>
+      <p>Your Settings</p>
+      <from>
+        <input type="text" value="https://api.realworld.io/images/smiley-cyrus.jpeg"/>
+        <input type="text"/>
+        <input placeholder="short bio about you " style={{height:"100px"}}/>
+        <input type="text"/>
+        <input type="text" placeholder='New Password'/>
+      </from>
+      <button className='btn1'> Update Settings</button>
+      
+      <button className='btn2'>Or Click Here To Logout</button>
     </div>
   )
 }

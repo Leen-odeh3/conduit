5fe82188e9904b38632496b743908ad8db1a2587
leen <leenodeh287@gmail.com>
2023-10-13 23:04:02 +0300

    signin style done

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,8 +1,8 @@
 import { NavLink } from "react-router-dom";
 import "./Header.css";
-import SettingsIcon from '@mui/icons-material/Settings';
-import AddLinkIcon from '@mui/icons-material/AddLink';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+// import SettingsIcon from '@mui/icons-material/Settings';
+// import AddLinkIcon from '@mui/icons-material/AddLink';
+// import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
 const Header = () => {
   return (
@@ -18,11 +18,11 @@ const Header = () => {
       <h1 style={{ color: "#5cb85c", paddingLeft: "5px" }}>Conduit</h1>
       <nav>
         <NavLink to="/">Home</NavLink>
-        {/* <NavLink to="/signin">Signin</NavLink>
-        <NavLink to="/signup">Signup</NavLink> */}
-        <NavLink to="/newpost"><AddLinkIcon/> NewPost</NavLink>
+        <NavLink to="/signin">Signin</NavLink>
+        <NavLink to="/signup">Signup</NavLink>
+        {/* <NavLink to="/newpost"><AddLinkIcon/> NewPost</NavLink>
         <NavLink to="/settings"> <SettingsIcon/>Settings</NavLink>
-        <NavLink to="/profile"><ManageAccountsIcon/> Profile</NavLink>
+        <NavLink to="/profile"><ManageAccountsIcon/> Profile</NavLink> */}
       </nav>
     </div>
   );
--- src/Pages/Signin/Signin.jsx
+++ src/Pages/Signin/Signin.jsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom";
 
 const Signin = () => {
   return (
-    <div>
-      signin
+    <div style={{ height: "500px", textAlign: "center", marginTop: "30px" }}>
+      <p style={{ textAlign: "center" }}>Sign In</p>
+      <Link to="/signup" style={{ color: "#5cb85c" }}>
+        Need an account?
+      </Link>
+      <from>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+      </from>
+      <button
+        className="btn1"
+        style={{ marginBottom: "20px", cursor: "pointer" }}
+      >
+        Sign In
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;

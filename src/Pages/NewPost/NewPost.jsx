799342e60db9c4e1a78ec370b0f41e1d24583fcb
leen <leenodeh287@gmail.com>
2023-10-13 22:31:16 +0300

    Newpost page done

--- src/Pages/NewPost/NewPost.jsx
+++ src/Pages/NewPost/NewPost.jsx
@@ -1,10 +1,35 @@
-
 const NewPost = () => {
   return (
     <div>
-      newpost
+      <form style={{ width: "100%", padding: "30px", margin: "auto" }}>
+        <input type="text" placeholder="Articl Title" />
+        <input type="text" placeholder="Whats this article about" />
+        <input
+          type="text"
+          placeholder="Write your article (in markdown)"
+          style={{ height: "100px" }}
+        />
+        <input type="text" placeholder="Enter tags" />
+        <button
+          style={{
+            color: "white",
+            border: "none",
+            backgroundColor: "#5cb85c",
+            marginBottom: "20px",
+            borderRadius: "8px",
+            padding: "16px 30px",
+            cursor: "pointer",
+            margin: "auto",
+            fontSize: "20px",
+          marginLeft:"45%",
+          marginTop:"20px"
+          }}
+        >
+          Publish Articls
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewPost
+export default NewPost;

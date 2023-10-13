9aec92626ac7854e4c6ded253d7eaf1fdb702314
leen <leenodeh287@gmail.com>
2023-10-13 23:06:42 +0300

    signup style-page is done ..

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
     <div>
-      signup
+      <div style={{ height: "500px", textAlign: "center", marginTop: "30px" }}>
+        <p style={{ textAlign: "center" }}>Sign In</p>
+        <Link to="/signup" style={{ color: "#5cb85c" }}>
+          Have an account?
+        </Link>
+        <from>
+          <input type="text" placeholder="UserName" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </from>
+        <button
+          className="btn1"
+          style={{ marginBottom: "20px", cursor: "pointer" }}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
